81bb19a1351df1fe0f0bd0c72baee525e7287cca
Jonathan Morel <jonathan.morel76@gmail.com>
2026-06-26 08:23:31 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE='cdm2026-v2';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./flags.js',
+  './oswald-500.woff2','./oswald-700.woff2','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{
+    const cp=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,cp)).catch(()=>{}); return res;
+  }).catch(()=>caches.match('./index.html'))));
+});
